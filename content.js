3a883230897e5d3a88aa2a047fdba355621b7c2f
ElectricAvenue22 <30714020+ElectricAvenue22@users.noreply.github.com>
2018-12-03 01:15:07 -0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,138 @@
+//queue up video button css style 
+const buttonCss = `font-weight: 800;
+            font-size: 19px;
+            background-color: red;
+            color: white;
+            z-index: 2000;
+            opacity: 0.9;
+            width: 32px;
+            height: 32px;
+            cursor: pointer;`
+
+//skip button css style 			
+const skipButtonCss = `font-weight: 800;
+            font-size: 19px;
+            background-color: red;
+            color: white;
+            z-index: 2000;
+            opacity: 0.15;
+            width: 50px;
+            height: 32px;
+			position: absolute;
+			left: 0px;
+			top: 0px;
+            cursor: pointer;`
+
+const timeInterval = 1000; //update interval between button checks 
+const playerload = 1000; //time we wait before attempting to add skip button to player, required so the player is loaded before we add it 
+
+addButtons(); //add buttons on page load 
+
+//every time out interval check for new buttons 
+function timeOutEvent(){
+    addButtons();
+    window.setTimeout(timeOutEvent, timeInterval);
+}
+
+window.setTimeout(timeOutEvent, timeInterval);
+
+function addButtons() {
+	
+    let links = document.getElementsByTagName('ytd-thumbnail');
+    for (i in links) {
+		if(links[i].id == null) continue; 
+		let children = links[i].childNodes;
+		let foundChild = false; 
+		for (child in children){
+			if(children[child].id == 'queueButton'){
+				children[child].setAttribute("href", links[i].getElementsByTagName('a')[0].href);
+				foundChild = true; 
+				break; 
+			}
+		}
+	    if (!foundChild) {
+            let node = document.createElement("button");
+            let bImage = document.createTextNode("+");
+            node.appendChild(bImage);
+            node.setAttribute("style", buttonCss);
+            node.setAttribute("href", links[i].getElementsByTagName('a')[0].href);
+			node.setAttribute("id", "queueButton");
+            node.onclick = function (event) { //add video to the queue 
+                chrome.runtime.sendMessage({ greeting: "VideoAdded", link: this.getAttribute("href") }, function () { });
+            }
+            links[i].appendChild(node); 
+        }
+    }
+}
+
+//sends message to skip the current video 
+function skipVideo(){
+	chrome.runtime.sendMessage({greeting : "Pop"}, function(){});
+}
+
+
+//add a skip button to the youtube player
+function addSkipButton(){ 
+	var youtubeVideoPlayer = document.getElementsByClassName('style-scope ytd-watch-flexy');
+	youtubeVideoPlayerLarge = youtubeVideoPlayer[6]; //6 theater and full screen viewer
+    youtubeVideoPlayerSmall = youtubeVideoPlayer[11]; //11 normal sized viewer 
+    youtubeVideoPlayerMini = youtubeVideoPlayer[14]; //14 is the mini player 
+	
+	//button 1 (large)
+	if(youtubeVideoPlayerLarge != null){
+		let bImageLarge = document.createTextNode(">>");
+		let skipButtonLarge = document.createElement("button");
+		skipButtonLarge.appendChild(bImageLarge);
+		skipButtonLarge.setAttribute("style", skipButtonCss);
+		skipButtonLarge.onclick = skipVideo;
+		skipButtonLarge.onmouseenter = function(event) {skipButtonLarge.style.opacity = 1;}
+		skipButtonLarge.onmouseleave = function(event) {skipButtonLarge.style.opacity = 0.15;}
+		youtubeVideoPlayerLarge.appendChild(skipButtonLarge);
+	}
+	
+	//button 2 (theater) 
+	if(youtubeVideoPlayerSmall != null){
+		let bImageSmall = document.createTextNode(">>");
+		var skipButtonSmall = document.createElement("button");
+		skipButtonSmall.appendChild(bImageSmall);
+		skipButtonSmall.setAttribute("style", skipButtonCss);
+		skipButtonSmall.onclick = skipVideo;
+		skipButtonSmall.onmouseenter = function(event) {skipButtonSmall.style.opacity = 1;}
+		skipButtonSmall.onmouseleave = function(event) {skipButtonSmall.style.opacity = 0.15;}
+		youtubeVideoPlayerSmall.appendChild(skipButtonSmall);
+	}
+    
+    //button 3 (mini)
+	if(youtubeVideoPlayerMini != null){
+		let bImageSmallx = document.createTextNode(">>");
+		var skipButtonSmallx = document.createElement("button");
+		skipButtonSmallx.appendChild(bImageSmallx);
+		skipButtonSmallx.setAttribute("style", skipButtonCss);
+		skipButtonSmallx.onclick = skipVideo;
+		skipButtonSmallx.onmouseenter = function(event) {skipButtonSmallx.style.opacity = 1;}
+		skipButtonSmallx.onmouseleave = function(event) {skipButtonSmallx.style.opacity = 0.15;}
+		youtubeVideoPlayerMini.appendChild(skipButtonSmallx);
+	}
+    
+    
+}
+
+//Wait for page to load before adding the skip buttons 
+window.setTimeout(addSkipButton, playerload); 
+
+
+//detects when a video ends 
+var video = document.getElementsByTagName("video")[0];
+if(video != null) {
+  video.addEventListener("ended", function() {
+  	chrome.runtime.sendMessage({greeting : "Pop"}, function(){});
+  });
+}
+
+/*
+    Protocol 
+
+    1. Popup.js request queue from content.js
+    2. Content.js responds with the queue and then clears the queue 
+    3. Popup.js appends requested videos to internal queue 
+*/
---
+++ popup.js
@@ -0,0 +1,164 @@
+//get button references 
+let play = document.getElementById('play');
+let deleteQueue = document.getElementById('deleteQueue');
+let skip = document.getElementById('skip');
+
+requestQueue(true); //get video queue from VideoManager
+
+//play the queue
+play.onclick = function (element) {
+  if(videoQueue.length > 0){
+    chrome.runtime.sendMessage({greeting: "Pop", newTab: true}, function(){})
+  }
+};
+
+//delete the entire queue
+deleteQueue.onclick = function (element) {
+  chrome.runtime.sendMessage({greeting: "Purge"}, function(){})
+  close();
+};
+  
+//skip to the next video in the queue
+skip.onclick = function (element) {
+  if(videoQueue.length > 0){
+    chrome.runtime.sendMessage({greeting: "Pop", newTab: false}, function(){})
+    close();
+  }
+}
+
+//get queue from VideoManager
+function requestQueue(repopQueue){
+    chrome.runtime.sendMessage({greeting: "RequestingQueue"}, function(response){
+        if(response.farewell == "QueueSent"){
+            videoQueue = response.queue;
+            if(repopQueue)
+                populateVideoScroller(videoQueue);
+        }
+    });
+}
+
+
+//thumbnail style sheet 
+const cssT = `
+      height: 75px;
+      margin-left: auto;
+      margin-right: auto;
+      margin-bottom: 10px;
+      position: relative;
+      left: 50px;`
+
+//Arrow button style sheet
+const upStyle = `
+      width: 22px;
+      margin-left: auto;
+      margin-right: auto;
+      margin-bottom: 2px;
+      position: relative;
+      right: 50px;
+      bottom: 62px;
+      opacity: 0.75;`
+
+//Arrow button style sheet
+const downStyle = `
+      width: 22px;
+      margin-left: auto;
+      margin-right: auto;
+      margin-bottom: 2px;
+      position: relative;
+      right: 72px;
+      bottom: 40px;
+      opacity: 0.75;`
+
+
+//add youtube thumbnails to the video scroller div 
+function populateVideoScroller(videoQueue){
+   
+   //clear the current contents 
+   let vidScroller = document.getElementById("videoBox");
+   vidScroller.innerHTML = "";   
+   
+  for(videoURL in videoQueue){
+
+    //Creating video thumbnail button
+    let elem = document.createElement("input");
+    elem.setAttribute("style", cssT);
+    elem.setAttribute("type", "image");
+    elem.setAttribute("href", videoQueue[videoURL]);
+    elem.src = urlToThumbail(videoQueue[videoURL]);
+
+    //show delete icon when hovered 
+    elem.onmouseenter = function(event) {
+      elem.src = "images/remove.png";
+    }
+    
+    //reset image on mouse leave 
+    elem.onmouseleave = function(event) {
+      elem.src = urlToThumbail(elem.getAttribute("href"));
+    }
+
+    //delete the clicked video from the queue 
+    elem.onclick = function(event) {
+      chrome.runtime.sendMessage({greeting: "Delete", url : elem.getAttribute("href")}, function(){});
+      elem.parentElement.remove();
+      requestQueue(false);
+    }
+
+    //Creating up and down arrow buttons
+    let upArr = document.createElement("input");
+    upArr.setAttribute("style", upStyle);
+    upArr.setAttribute("type", "image");
+    upArr.src = "images/Up.png";
+    let downArr = document.createElement("input");
+    downArr.setAttribute("style", downStyle);
+    downArr.setAttribute("type", "image");
+    downArr.src = "images/Down.png";
+
+    //changing opacities when hovering over arrows
+    upArr.onmouseenter = function(event){ upArr.style.opacity = 1; }
+    upArr.onmouseleave = function(event){ upArr.style.opacity = 0.75; }
+    downArr.onmouseenter = function(event){ downArr.style.opacity = 1; }
+    downArr.onmouseleave = function(event){ downArr.style.opacity = 0.75; }
+
+    //Moving video in queue when clicking an arrow
+    upArr.onclick = function(event){
+      chrome.runtime.sendMessage({greeting: "Move", dir: "Up", link: elem.getAttribute("href")}, function(response){
+        if(response.farewell == "Swapped"){
+          requestQueue(true);
+        }
+      });
+    }
+    
+    downArr.onclick = function(event){
+      chrome.runtime.sendMessage({greeting: "Move", dir: "Down", link: elem.getAttribute("href")}, function(response){
+        if(response.farewell == "Swapped"){
+          requestQueue(true);
+        }
+      });
+    }
+
+    //Appending elements to the video scroller
+    let vidCont = document.createElement("VideoCont");
+    vidCont.appendChild(elem);
+    vidCont.appendChild(upArr);
+    vidCont.appendChild(downArr);
+    vidScroller.appendChild(vidCont);
+  }
+}
+
+//converts a youtube video url to a url that references the thumbnail image 
+function urlToThumbail(youtubeUrl){
+  let head = "http://img.youtube.com/vi/"
+  let tail ="/0.jpg";
+  let link = ""
+  let numEquals = 0;
+  for(i = 0; i < youtubeUrl.length && numEquals < 2; i++){
+    if(youtubeUrl[i] == "=" || youtubeUrl[i] == "&"){
+      numEquals++;
+    } else if(numEquals == 1){
+      link+= youtubeUrl[i];
+    }
+  }
+  return head + link + tail;
+}
+
+
