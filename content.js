ab6b6e3e0513fd93e7bbe2f8bf29cc16dfeb50e4
ElectricAvenue22 <mitchel0022@gmail.com>
2019-03-30 21:42:05 -0400

    Fixed some formatting issues with the github code viewer

--- content.js
+++ content.js
@@ -9,79 +9,80 @@ const buttonCss = `font-weight: 800;
     height: 32px;
     cursor: pointer;`
 
-//skip button css style 			
+//skip button css style             
 const skipButtonCss = `font-weight: 800;
     font-size: 19px;
     background-color: red;
     color: white;
     z-index: 2000;
     opacity: 0.15;
-	width: 50px;
+    width: 50px;
     height: 32px;
-	position: absolute;
-	left: 0px;
-	top: 0px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
     cursor: pointer;`
 
-const timeInterval = 1000; //update interval between button checks 
-const playerload = 1000; //time we wait before attempting to add skip button to player, required so the player is loaded before we add it 
+const TIME_INTERVAL = 1000; //update interval between button checks 
+const PLAYER_LOAD = 1000; //time we wait before attempting to add skip button to player, required so the player is loaded before we add it 
 
 addButtons(); //add buttons on page load 
 
 //every time out interval check for new buttons 
 function timeOutEvent(){
     addButtons();
-    window.setTimeout(timeOutEvent, timeInterval);
+    window.setTimeout(timeOutEvent, TIME_INTERVAL);
 }
 
 let full = true;
 
-window.setTimeout(timeOutEvent, timeInterval);
+window.setTimeout(timeOutEvent, TIME_INTERVAL);
 
 //if fullscreen mode is active cleanup URL to point to embeded player 
 function createLink(fullScreenLink, node){
-	let link = "";
-	if(fullScreenLink){
-		link = node.getAttribute("href").replace('watch?v=', 'embed/')
-		//if there is still an equal sign delete it 
-		if(link.indexOf('=') != -1){
-			link = link.substr(0,link.indexOf('&t='));
-		}
-		link += '?rel=0&amp;autoplay=1;fs=1;autohide=1;hd=1;'//full screen modifiers
-	}else{
-		link = node.getAttribute("href");
-	}
-	return link;
+    let link = "";
+    if(fullScreenLink){
+        link = node.getAttribute("href").replace('watch?v=', 'embed/')
+        //if there is still an equal sign delete it 
+        if(link.indexOf('=') != -1){
+            link = link.substr(0,link.indexOf('&t='));
+        }
+        link += '?rel=0&amp;autoplay=1;fs=1;autohide=1;hd=1;'//full screen modifiers
+    }else{
+        link = node.getAttribute("href");
+    }
+    return link;
 }
 
 //Adds the '+' buttons to all the currently visible youtube videos 
 function addButtons() {
     let links = document.getElementsByTagName('ytd-thumbnail');
     for (i in links) {
-		if(links[i].id == null) continue; 
-		let children = links[i].childNodes;
-		let foundChild = false; 
-		for (child in children){
-			if(children[child].id == 'queueButton'){
-				children[child].setAttribute("href", links[i].getElementsByTagName('a')[0].href);
-				foundChild = true; 
-				break; 
-			}
-		}
-		//A button does not yet exist so add one 
-	    if (!foundChild) {
-
-			let forceFullScreen = false;
-			chrome.storage.sync.get(['fullscreen'], function(result){ forceFullScreen = result.fullscreen });
+        if(links[i].id == null) continue; 
+        let children = links[i].childNodes;
+        let foundChild = false; 
+        for (child in children){
+            if(children[child].id == 'queueButton'){
+                children[child].setAttribute("href", links[i].getElementsByTagName('a')[0].href);
+                foundChild = true; 
+                break; 
+            }
+        }
+        //A button does not yet exist so add one 
+        if (!foundChild) {
+			
+			//Get full screen status 
+            let forceFullScreen = false;
+            chrome.storage.sync.get(['fullscreen'], function(result){ forceFullScreen = result.fullscreen });
 
             let node = document.createElement("button");
-			let bImage = document.createTextNode("+");
+            let bImage = document.createTextNode("+");
             node.appendChild(bImage);
             node.setAttribute("style", buttonCss);
             node.setAttribute("href", links[i].getElementsByTagName('a')[0].href);
-			node.setAttribute("id", "queueButton");
-			node.onclick = function (event) { //add video to the queue 
-				chrome.runtime.sendMessage({ greeting: "VideoAdded", link: createLink(forceFullScreen, node)}, function() { });
+            node.setAttribute("id", "queueButton");
+            node.onclick = function (event) { //add video to the queue 
+                chrome.runtime.sendMessage({ greeting: "VideoAdded", link: createLink(forceFullScreen, node)}, function() { });
             }
             links[i].appendChild(node); 
         }
@@ -90,68 +91,68 @@ function addButtons() {
 
 //sends message to skip the current video 
 function skipVideo(){
-	chrome.runtime.sendMessage({greeting : "Pop"}, function(){});
+    chrome.runtime.sendMessage({greeting : "Pop"}, function(){});
 }
 
-
+//returns a new instance of a skip button 
 function createNewSkipButton(){
-	let bImageLarge = document.createTextNode(">>");
-	let skipButton = document.createElement("button");
-	skipButton.appendChild(bImageLarge);
-	skipButton.setAttribute("style", skipButtonCss);
-	skipButton.onclick = skipVideo;
-	skipButton.onmouseenter = function(event) {skipButton.style.opacity = 1;}
-	skipButton.onmouseleave = function(event) {skipButton.style.opacity = 0.15;}
-	return skipButton;
+    let bImageLarge = document.createTextNode(">>");
+    let skipButton = document.createElement("button");
+    skipButton.appendChild(bImageLarge);
+    skipButton.setAttribute("style", skipButtonCss);
+    skipButton.onclick = skipVideo;
+    skipButton.onmouseenter = function(event) {skipButton.style.opacity = 1;}
+    skipButton.onmouseleave = function(event) {skipButton.style.opacity = 0.15;}
+    return skipButton;
 }
 
 //add a skip button to the youtube player
 function addSkipButton(){ 
-	var youtubeVideoPlayer = document.getElementsByClassName('style-scope ytd-watch-flexy');
-	youtubeVideoPlayerLarge = youtubeVideoPlayer[6]; //6 theater and full screen viewer
+    var youtubeVideoPlayer = document.getElementsByClassName('style-scope ytd-watch-flexy');
+    youtubeVideoPlayerLarge = youtubeVideoPlayer[6]; //6 theater and full screen viewer
     youtubeVideoPlayerSmall = youtubeVideoPlayer[11]; //11 normal sized viewer 
-	youtubeVideoPlayerMini = youtubeVideoPlayer[14]; //14 is the mini player 
-	yotubeVideoPlayerTheater = document.getElementById('player');
-	
-	//button 1 (large)
-	if(youtubeVideoPlayerLarge != null){
-		skipButtonLarge = createNewSkipButton()
-		youtubeVideoPlayerLarge.appendChild(skipButtonLarge);
-	}
-	
-	//button 2 (small) 
-	if(youtubeVideoPlayerSmall != null){
-		skipButtonSmall = createNewSkipButton()
-		youtubeVideoPlayerSmall.appendChild(skipButtonSmall);
-	}
+    youtubeVideoPlayerMini = youtubeVideoPlayer[14]; //14 is the mini player 
+    yotubeVideoPlayerTheater = document.getElementById('player');
+    
+    //button 1 (large)
+    if(youtubeVideoPlayerLarge != null){
+        skipButtonLarge = createNewSkipButton()
+        youtubeVideoPlayerLarge.appendChild(skipButtonLarge);
+    }
+    
+    //button 2 (small) 
+    if(youtubeVideoPlayerSmall != null){
+        skipButtonSmall = createNewSkipButton()
+        youtubeVideoPlayerSmall.appendChild(skipButtonSmall);
+    }
     
     //button 3 (mini)
-	if(youtubeVideoPlayerMini != null){
-		skipButtonMini = createNewSkipButton()
-		youtubeVideoPlayerMini.appendChild(skipButtonMini);
-	}
+    if(youtubeVideoPlayerMini != null){
+        skipButtonMini = createNewSkipButton()
+        youtubeVideoPlayerMini.appendChild(skipButtonMini);
+    }
 
-	//button 4 (theater)
-	if(yotubeVideoPlayerTheater != null){
-		skipButtonTheater = createNewSkipButton()
-		yotubeVideoPlayerTheater.appendChild(skipButtonTheater);
-	}
+    //button 4 (theater)
+    if(yotubeVideoPlayerTheater != null){
+        skipButtonTheater = createNewSkipButton()
+        yotubeVideoPlayerTheater.appendChild(skipButtonTheater);
+    }
     
     
 }
 
 //Wait for page to load before adding the skip buttons 
 chrome.storage.sync.get(['showskip'], function(result){ 
-	if(result.showskip){
-		window.setTimeout(addSkipButton, playerload); 
-	}
+    if(result.showskip){
+        window.setTimeout(addSkipButton, PLAYER_LOAD); 
+    }
 });
 
 //detects when a video ends 
 var video = document.getElementsByTagName("video")[0];
 if(video != null) {
   video.addEventListener("ended", function() {
-  	chrome.runtime.sendMessage({greeting : "Pop"}, function(){});
+      chrome.runtime.sendMessage({greeting : "Pop"}, function(){});
   });
 }
 
